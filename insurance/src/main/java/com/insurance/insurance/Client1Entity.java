a6def766a3059b21d63e3a0bdc9bd384c07ac47b
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-10-21 14:43:08 +0530

    Commit for User Input (end point)

--- insurance/src/main/java/com/insurance/insurance/Client1Entity.java
+++ insurance/src/main/java/com/insurance/insurance/Client1Entity.java
@@ -8,47 +8,47 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="client1")
-
+@Table(name = "client1")
 
 public class Client1Entity {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-private Integer insurance_id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer insurance_id;
 	private String Client_Name;
 	private Integer Term_Plan;
 	private String Maturity_Date;
+
 	public Integer getInsurance_id() {
 		return insurance_id;
 	}
+
 	public void setInsurance_id(Integer insurance_id) {
 		this.insurance_id = insurance_id;
 	}
+
 	public String getClient_Name() {
 		return Client_Name;
 	}
+
 	public void setClient_Name(String client_Name) {
 		Client_Name = client_Name;
 	}
+
 	public Integer getTerm_Plan() {
 		return Term_Plan;
 	}
+
 	public void setTerm_Plan(Integer term_Plan) {
 		Term_Plan = term_Plan;
 	}
+
 	public String getMaturity_Date() {
 		return Maturity_Date;
 	}
+
 	public void setMaturity_Date(String maturity_Date) {
 		Maturity_Date = maturity_Date;
 	}
-	
-	
-
-	
-
-
-
 
 }
--- insurance/src/main/java/com/insurance/insurance/ClientController.java
+++ insurance/src/main/java/com/insurance/insurance/ClientController.java
@@ -1,9 +1,14 @@
 package com.insurance.insurance;
 
 import java.util.List;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,55 +16,85 @@ public class ClientController {
 
 	@Autowired
 	private Client1Repo cl;
-	
 
+	@GetMapping("/detail")
+	public List<Client1Entity> getSample() {
 
+		return cl.getjoin();
+	}
 
+	@GetMapping("/detail/10263")
+	public List<Client1Entity> getSample1() {
 
-    @GetMapping("/detail")
- public List<Client1Entity> getSample(){
-    	
-    	
-    	 	
-    	return cl.getjoin();
-    }
-    
-    @GetMapping("/detail/10263")
- public List<Client1Entity> getSample1(){
-    	
-    	
-    	return cl.getjoin1();
-    }
-    @GetMapping("/detail/10265")
- public List<Client1Entity> getSample2(){
-    	
-    	
-    	return cl.getjoin2();
-    }
-    
-    @GetMapping("/detail/10267")
- public List<Client1Entity> getSample3(){
-    	
-    	
-    	return cl.getjoin3();
-    }
-    
-    @GetMapping("/detail/50230")
- public List<Client1Entity> getSample4(){
-    	
-    	
-    	return cl.getjoin4();
-    }
-    @GetMapping("/detail/50236")
- public List<Client1Entity> getSample5(){
-    	
-    	
-    	return cl.getjoin5();
-    }
-    
-    
-    
+		return cl.getjoin1();
+	}
 
-    }
-    	
-	
+	@GetMapping("/detail/10265")
+	public List<Client1Entity> getSample2() {
+
+		return cl.getjoin2();
+	}
+
+	@GetMapping("/detail/10267")
+	public List<Client1Entity> getSample3() {
+
+		return cl.getjoin3();
+	}
+
+	@GetMapping("/detail/50230")
+	public List<Client1Entity> getSample4() {
+
+		return cl.getjoin4();
+	}
+
+	@GetMapping("/detail/50236")
+	public List<Client1Entity> getSample5() {
+
+		return cl.getjoin5();
+	}
+
+	@GetMapping("/equaltofive")
+	public List<Client1Entity> getSample6() {
+
+		return cl.getjoin6();
+	}
+
+	@GetMapping("/morethanfive")
+	public List<Client1Entity> getSample7() {
+
+		return cl.getjoin7();
+	}
+
+	@GetMapping("/lessthanfive")
+	public List<Client1Entity> getSample8() {
+
+		return cl.getjoin8();
+	}
+
+	@GetMapping("/{term_plan}/{discount}")
+
+	public int discount(@PathVariable String term_plan, @PathVariable int discount) {
+
+		final String more = "morethanfive";
+		final String less = "lessthanfive";
+		final String equal = "equaltofive";
+
+		int per = 0;
+
+		if (term_plan.equals(less)) {
+
+		}
+
+		else if (term_plan.equals(equal)) {
+
+			per = 10 * discount / 100;
+
+		} else if (term_plan.equals(more)) {
+
+			per = 12 * discount / 100;
+
+		}
+		return discount - per;
+
+	}
+}
