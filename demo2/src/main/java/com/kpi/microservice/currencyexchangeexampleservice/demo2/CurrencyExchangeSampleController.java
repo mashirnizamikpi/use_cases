b16a0c266ef4be66903155a6d30266f45f38c6b1
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-10-27 19:44:33 +0530

    Commit

--- demo2/src/main/java/com/kpi/microservice/currencyexchangeexampleservice/demo2/CurrencyExchangeSampleController.java
+++ demo2/src/main/java/com/kpi/microservice/currencyexchangeexampleservice/demo2/CurrencyExchangeSampleController.java
@@ -1,16 +1,8 @@
 package com.kpi.microservice.currencyexchangeexampleservice.demo2;
-
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyExchangeSampleController {
